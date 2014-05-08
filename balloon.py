a83ad4c8642a00d8533dac76c49a27ea496f6273
Arthur Benemann <arthur.benemann@gmail.com>
2014-05-08 16:56:02 -0700

    Implementing a simple Kalman Filter

--- balloon.py
+++ balloon.py
@@ -12,6 +12,9 @@ import cv2.cv as cv
 import video
 import math
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+
+from pykalman import KalmanFilter
 
 if __name__ == '__main__':
 
@@ -27,7 +30,7 @@ if __name__ == '__main__':
     h_high = 10
     s_low = 120
     s_high = 250
-    v_low = 170
+    v_low = 130
     v_high = 255
 
     # call back for trackbar movements
@@ -47,7 +50,7 @@ if __name__ == '__main__':
 
     #Capture a single frame just to show the result window first
     flag, frame = cam.read()
-    cv2.imshow('result',frame);
+#    cv2.imshow('result',frame);
 
     #Display the plot
     fig=plt.figure()
@@ -55,11 +58,20 @@ if __name__ == '__main__':
     plt.hold(False)
 
 	
+    #Init Kalman
+
+    A = np.array([[1, 0], [0, 1]])	# Process model
+    H = np.array([[1, 0], [0, 1]])	# Measurament model
+    Q = np.eye(2) *0.002		# Process covariance
+    R = np.array([[5, 0], [0, 1]])	# Measurament covariance
+    x = np.array([0, 0])		# Initial pos
+    P = np.eye(2)*100000000		# Initial Covariance
+    z = np.array([0,0])			# Dummy init
 
     while True:
 	# Read frame
         flag, frame = cam.read()
-        cv2.imshow('camera', frame)
+#        cv2.imshow('camera', frame)
         small = cv2.pyrDown(frame)
 	small = frame
 
@@ -97,13 +109,11 @@ if __name__ == '__main__':
 	#Transform to Gray scale
         img = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 
-	cv2.imshow('mask',res)
+#	cv2.imshow('mask',res)
 
 	#Blur and Hough
 	img = cv2.medianBlur(img, 5)
-
 	circles = cv2.HoughCircles(img, cv.CV_HOUGH_GRADIENT, 2, 200, np.array([]), 100, 30, 1, 200)
-
 
         # draw circles around the circles
 	cimg = small.copy() # numpy function
@@ -116,29 +126,51 @@ if __name__ == '__main__':
                 # draw the center of the circle
                 cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
-		print('x:'+str(i[0]).rjust(3),'y:'+str(i[1]).rjust(3),'size:'+str(i[2]).rjust(3),'-'*i[2])
-
 		#Calculate measurament variables
 		u= (i[0]/320.0)-1
-		pho = 20.0/i[2]
+		pho = 20.0/i[2]		
+		z = [pho*1.0/math.sqrt(1+u*u), pho*u/math.sqrt(1+u*u)]
 
-		
-		x_ = pho*1.0/math.sqrt(1+u*u)
-		y_ = pho*u/math.sqrt(1+u*u)
-
-
-		print(x_,y_)
-		plt.plot(x_, y_, 'ro')
-
-           	plt.axis([0,2,-1,1])
-		plt.draw()
-
+		#print('x:'+str(i[0]).rjust(3),'y:'+str(i[1]).rjust(3),'size:'+str(i[2]).rjust(3),'-'*i[2])
 		break
 	else:
 		print('-')
+
+
+
+
+
+	#Kalman Predict
+	xp = A.dot(x)
+	Pp = A.dot(P.dot(A.T)) + Q
+
+	if not (circles is None):
+		#Kalman Update
+		K = Pp.dot(H.T.dot(np.linalg.inv(H.dot(Pp.dot(H.T)) + R)))		
+		x = xp + K.dot(z - H.dot(xp))
+		P = Pp - K.dot(H.dot(Pp))			
+	else:
+		P = Pp
+
+	#print(z)
+	#print(x)
+
+	#Plot
+	if not (circles is None):
+		plt.plot(z[0],z[1], 'ro', x[0],x[1], 'bo')
+	else:
+		plt.plot(x[0],x[1], 'bo')
+      	plt.axis([0,2,-1,1])
+	
+	if (P.max() <= 1):
+		plt.gca().add_patch(Ellipse(xy=(x[0], x[1]), width=P[0,0], height=P[1,1], 
+                        edgecolor='b', fc='None', lw=2))
+	plt.draw()
+	print(P)
+	print(P.max() <= 1)
+	print(x)
+
 	cv2.imshow('result',cimg)
-
-
 	
 
 
